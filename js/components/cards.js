45f05d05e36653daea283abad9a8013b6c155058
paulinavita <paulinadavita@gmail.com>
2019-04-12 09:31:31 +0700

    changes card not signifc

--- js/components/cards.js
+++ js/components/cards.js
@@ -7,11 +7,13 @@ Vue.component('image-card', {
 
     },
     template : 
-    `<div >
+    `<div>
+    <div class="mt-4 row">
     <div v-for="i in 3" class="card" style="width: 18rem;">
     <img class="card-img-top" src="..." alt="Card image cap">
     <div class="card-body">
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
     </div>
   </div>
   </div>`
